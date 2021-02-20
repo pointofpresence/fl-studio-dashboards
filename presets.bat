1aee9b9e94e84792c753be7f99c03049432b34b3
resampled <git@pointofpresence.ru>
2021-02-21 03:22:07 +0700

    Home

---
+++ presets.bat
@@ -0,0 +1,3 @@
+robocopy "C:\Users\ReSampled\Documents\Image-Line\Data\FL Studio\Presets\Plugin presets\Generators\MIDI Out" "H:\FL Studio 20\Plugins\Fruity\Generators\Dashboard\MIDI Out" /e
+robocopy "C:\Users\ReSampled\Documents\Image-Line\Data\FL Studio\Presets\Plugin presets\Generators\Dashboard" "H:\FL Studio 20\Plugins\Fruity\Generators\Dashboard\Dashboard" /e
+
