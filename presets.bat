84bb3690a6eb8b3b87bb6c427abe66d0fed1f6ca
resampled <git@pointofpresence.ru>
2021-03-06 02:53:44 +0700

    roland fa-06/07/08 tone dashboard

--- presets.bat
+++ presets.bat
@@ -1,3 +1,4 @@
 robocopy "C:\Users\ReSampled\Documents\Image-Line\Data\FL Studio\Presets\Plugin presets\Generators\MIDI Out" "H:\FL Studio 20\Plugins\Fruity\Generators\Dashboard\MIDI Out" /e
 robocopy "C:\Users\ReSampled\Documents\Image-Line\Data\FL Studio\Presets\Plugin presets\Generators\Dashboard" "H:\FL Studio 20\Plugins\Fruity\Generators\Dashboard\Dashboard" /e
 
+robocopy "C:\Users\ReSampled\Documents\Image-Line\Data\FL Studio\Presets\Plugin presets\Effects\Control Surface" "H:\FL Studio 20\Plugins\Fruity\Generators\Dashboard\Control Surface" /e
